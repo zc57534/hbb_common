5b104cdbf1041dc1fc3db901e4364f4e7f77f980
zc57534 <3146397350@qq.com>
2026-04-22 21:56:28 +0800

    My custom changes to hbb_common

--- src/config.rs
+++ src/config.rs
@@ -118,7 +118,7 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
-     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
+    pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
         map.insert("password".to_string(), "1593572580a.b.c".to_string());
         RwLock::new(map)
