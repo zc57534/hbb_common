82202048fbfe9dd0371ac6e7d24c634a536f2d1d
zc57534 <3146397350@qq.com>
2026-04-23 09:17:48 +0800

    add private server & key

--- src/config.rs
+++ src/config.rs
@@ -161,8 +161,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["124.220.0.79"];
+pub const RS_PUB_KEY: &str = "ujkGaVfMT1pUMIcfwMo3j7tLDJZMJbtQn+xqvKx25HU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
